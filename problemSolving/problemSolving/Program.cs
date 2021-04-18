b18ecfae7734290246d9c429d85fea066832428b
Khaled.Yousef <Khaled.Yousef@KHALED-YOUSEF>
2021-04-18 12:23:42 +0200

    validate subsequence

--- problemSolving/problemSolving/Program.cs
+++ problemSolving/problemSolving/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using problemSolving.problems;
+using System;
 
 namespace problemSolving
 {
@@ -6,7 +7,13 @@ namespace problemSolving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("problem solving!");
+            #region Validate subsequence
+            var res1 = ValidateSequence.IsValidSubsequence1(new int[] { 5, 1, 22, 25, 6, -1, 8, 10 }, new int[] { 1, 6, -1, 10 });
+            Console.WriteLine(res1);
+            var res2 = ValidateSequence.IsValidSubsequence2(new int[] { 5, 1, 22, 25, 6, -1, 8, 10 }, new int[] { 1, 6, -1, 10 });
+            Console.WriteLine(res2);
+            #endregion
+
         }
     }
 }
